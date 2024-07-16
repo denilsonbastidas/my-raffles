2dd5fb71ff081118c14dfa99292c33d1be642483
gustavogarcia-dev <gggl31128@gmail.com>
2024-07-16 18:48:47 -0400

    insert header

---
+++ src/components/HeaderAuth.tsx
@@ -0,0 +1,11 @@
+export default function HeaderAuth() {
+  return (
+    <header className="flex items-center bg-white border-b  shadow p-3 ">
+      <img
+        src="/logoCompany.svg"
+        className="h-12 w-14 overflow-hidden"
+        alt="company "
+      />
+    </header>
+  );
+}
