8d3af2f49accaf59abacfbbc528c16a831e9ea30
Denilson <denilson@socialgest.net>
2025-03-06 18:08:44 -0400

    changed colors in header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,19 +9,21 @@ interface Props {
 function HeaderPage({ name, description, images, ticketPrice }: Props) {
   return (
     <div className="flex flex-col w-full mx-auto justify-center items-center border-b border-gray-50 p-8 md:w-2/3">
-      <div className="flex flex-col mb-6 text-center">
-        <h1 className="text-4xl font-semibold">Bienvenido Futuro Ganador</h1>
-        <h4 className="text-lg text-gray-200 font-bold">
+      <div className="flex flex-col mb-6 gap-1 text-center border-b py-1">
+        <h1 className="text-4xl text-blue-200 font-semibold">
+          Bienvenido Futuro Ganador
+        </h1>
+        <h4 className="text-md text-gray-200 font-bold">
           Estas a un Paso de Ganar con Denilson Bastidas
         </h4>
       </div>
       <div className="flex flex-col text-center mb-3">
         <p className="text-4xl font-semibold text-gray-100 mb-2">{name}</p>
-        <p className="text-xl font-semibold text-gray-100 mb-2">
+        <p className="text-lg font-semibold text-gray-100 mb-2 md:text-xl">
           {description}
         </p>
         <p className="text-xl font-medium text-blue-200">
-          Por tan sólo {ticketPrice?.toString() || 0}$ por ticket.
+          Por tan sólo {ticketPrice?.toString() || 0}$ por tickets.
         </p>
       </div>
       {/* slider */}
